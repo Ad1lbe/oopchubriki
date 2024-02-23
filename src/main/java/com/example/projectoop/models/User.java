7ec773fe9e7ab25f25730880508c5b3f1744dfee
adilbek <231028@astanait.edu.kz>
2024-02-23 09:33:05 +0600

    new changes

--- src/main/java/com/example/projectoop/models/User.java
+++ src/main/java/com/example/projectoop/models/User.java
@@ -1,5 +1,15 @@
 package com.example.projectoop.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
 public class User {
 
 }
