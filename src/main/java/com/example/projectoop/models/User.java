01d27b76b445784210267386f9722d33c7a89d3f
rusyajkeee <230432@astanait.edu.kz>
2024-02-25 18:38:07 +0600

    fixed lombok usage

--- src/main/java/com/example/projectoop/models/User.java
+++ src/main/java/com/example/projectoop/models/User.java
@@ -3,10 +3,8 @@ package com.example.projectoop.models;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.boot.autoconfigure.web.WebProperties;
-@Getter
-@ToString
-@Setter
-@NoArgsConstructor
+
+
 @Data
 @Entity
 @Table(name = "users")
