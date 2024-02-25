5581cdbea9b81ef14447a264e8cf8eb9ee0ee562
rusyajkeee <230432@astanait.edu.kz>
2024-02-25 19:09:08 +0600

    UserService reborn

--- src/main/java/com/example/projectoop/services/UserService.java
+++ src/main/java/com/example/projectoop/services/UserService.java
@@ -1,4 +1,43 @@
 package com.example.projectoop.services;
 
-public class UserService {
+import com.example.projectoop.models.Ticket;
+import com.example.projectoop.models.User;
+import com.example.projectoop.repositories.UserRepositoryInterface;
+import com.example.projectoop.services.Interface.UserServiceInterface;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class UserService implements UserServiceInterface {
+
+    private final UserRepositoryInterface repo;
+
+    public UserService(UserRepositoryInterface repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return repo.findAll();
+    }
+
+    @Override
+    public User getById(int id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    @Override
+    public User create(User user) {
+        return null;
+    }
+
+    @Override
+    public User getByLogin(String login) {
+        return null;
+    }
+
+    @Override
+    public List<User> getByTicket(Ticket ticket) {
+        return null;
+    }
 }
