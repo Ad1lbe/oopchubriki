7d8d8b0015a4debe56d1b96eff72d389a8444ba5
rusyajkeee <230432@astanait.edu.kz>
2024-02-25 21:19:24 +0600

    fixed lombok usage and added owner's id to the class

--- src/main/java/com/example/projectoop/models/Ticket.java
+++ src/main/java/com/example/projectoop/models/Ticket.java
@@ -1,17 +1,21 @@
 package com.example.projectoop.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tickets")
 public class Ticket {
-    private static int id_gen=1;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String name;
     private int price;
-    private int id = id_gen++;
+    private int id;
+    private int owner;
 
 
 
