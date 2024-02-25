6f00c9621835d752549aa703ff685824c9aff537
rusyajkeee <230432@astanait.edu.kz>
2024-02-25 19:08:39 +0600

    getAll and getById methods

--- src/main/java/com/example/projectoop/controller/UserController.java
+++ src/main/java/com/example/projectoop/controller/UserController.java
@@ -1,16 +1,43 @@
 package com.example.projectoop.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.projectoop.models.User;
+import com.example.projectoop.services.Interface.UserServiceInterface;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("users")
 
 
 public class UserController {
+
+    private final UserServiceInterface service;
+
+    public UserController(UserServiceInterface service) {
+        this.service = service;
+    }
+
+
     @GetMapping
     public String sayHello(){
         return "Hello wordl";
+    }
+
+    @GetMapping("/")
+    public List<User> getAll(){
+        return service.getAll();
+    }
+
+    @GetMapping("/{user_id}")
+    public ResponseEntity<User> getById(@PathVariable("user_id") int id) {
+        User user = service.getById(id);
+        if(user == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
