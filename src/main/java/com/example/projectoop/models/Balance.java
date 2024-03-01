42167b0d1761031766f3f5708a631bd7194ad479
Waitan <231427@astanait.edu.kz>
2024-03-02 04:58:15 +0500

    модель баланса с которого можно будет тратить деньги

---
+++ src/main/java/com/example/projectoop/models/Balance.java
@@ -0,0 +1,23 @@
+package com.example.projectoop.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="balance")
+@Data
+@NoArgsConstructor
+AllArgsConstructor
+public class Balance {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private long id;
+private double balance;
+
+
+@ManyToOne
+@JoinColumn(name = "user_id", nullable = false)
+private User user;
+}
