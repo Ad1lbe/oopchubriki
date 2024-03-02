4e88d9d3a05c78501999ca1164ed1c17a6bffbb5
Waitan <231427@astanait.edu.kz>
2024-03-02 06:06:57 +0500

    created TransactionController but its empty

---
+++ src/main/java/com/example/projectoop/controller/TransactionController.java
@@ -0,0 +1,9 @@
+package com.example.projectoop.controller;
+
+import com.example.projectoop.exception.UserExistException;
+import com.example.projectoop.models.Transaction;
+import com.example.projectoop.models.User;
+
+
+public class TransactionController {
+}
